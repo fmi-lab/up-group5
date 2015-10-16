a9a2c17af998fd8e3dbc623cda5321f926ea5b7d
Monika Eftimova <meftimova@uni-sofia.bg>
2015-10-16 07:45:23 +0300

    Add solutions to problems 0, 1 and 2

---
+++ exercises/exercise1/solutions/problem0.cpp
@@ -0,0 +1,56 @@
+/* ======================================================
+ *  Problem0
+ * ------------------------------------------------------
+ *
+ * Input: coordinates of a point (x, y)
+ *
+ * Output: the quadrant of the coordinate system the
+ * point belongs to; add check if the point is lying on a
+ * coordinate axis
+ *
+ * ====================================================== */
+ 
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double x, y;
+
+    cout << "x = ";
+    cin >> x;
+
+    cout << "y = ";
+    cin >> y;
+
+    cout << "The point lies ";
+
+    if (x * y == 0)
+    {
+        cout << "on a coordinate axis." << endl;
+        return 0;
+    }
+
+    if (x > 0 && y > 0)
+    {
+        cout << "in the first quadrant." << endl;
+        return 0;
+    }
+
+    if (x < 0 && y > 0)
+    {
+        cout << "in the second quadrant." << endl;
+        return 0;
+    }   
+
+    if (x < 0 && y < 0)
+    {
+        cout << "in the third quadrant." << endl;
+        return 0;
+    }   
+
+    cout << "in the forth quadrant." << endl;
+    return 0;
+}
+
---
+++ exercises/exercise1/solutions/problem2.cpp
@@ -0,0 +1,27 @@
+/* ======================================================
+ *  Problem1
+ * ------------------------------------------------------
+ *
+ * Input: year
+ *
+ * Output: True/False; if the year is a leap year
+ *
+ * ====================================================== */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int year;
+
+    cout << "Enter year: ";
+    cin >> year;
+
+    bool is_leap = (year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0);
+
+    cout << boolalpha << is_leap << endl;
+
+    return 0;
+}
